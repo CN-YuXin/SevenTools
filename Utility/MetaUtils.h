0117ed8f0d8b5b428487c7cf063383194764290a
YuXin <YuXin_YuYe2024@outlook.com>
2025-08-08 13:17:41 +0800

    对上次提交新增的头文件增添内容

--- Utility/MetaUtils.h
+++ Utility/MetaUtils.h
@@ -1,21 +1,42 @@
 #ifndef MetaUtilsByYX
 #define MetaUtilsByYX
 
+#include <CppVersions.h>
 #include <cstddef>
 
-// 主模板仅在 Types 为空时被使用
-template <std::size_t idx, class... Types>
-struct TypeAt {
-    static_assert(idx < sizeof...(Types), "Idx too large");
-};
-template <std::size_t idx, class T, class... Types>
-struct TypeAt<idx, T, Types...> {
-    static_assert(idx < sizeof...(Types) + 1, "Idx too large");
-    using Type = typename TypeAt<idx - 1, Types...>::Type;
-};
-template <class T, class... Types>
-struct TypeAt<0, T, Types...> {
-    using Type = T;
-};
+namespace SevenTools {
+    // 主模板仅在 Types 为空时被使用
+    template <std::size_t idx, class... Types>
+    struct TypeAt {
+        static_assert(idx < sizeof...(Types), "Idx too large");
+    };
+    template <std::size_t idx, class T, class... Types>
+    struct TypeAt<idx, T, Types...> {
+        static_assert(idx < sizeof...(Types) + 1, "Idx too large");
+        using Type = typename TypeAt<idx - 1, Types...>::Type;
+    };
+    template <class T, class... Types>
+    struct TypeAt<0, T, Types...> {
+        using Type = T;
+    };
+    template <std::size_t idx, class... Types>
+    using TypeAtAlias = typename TypeAt<idx, Types...>::Type;
+
+
+    struct InPlace {explicit InPlace() = default;};
+    template <class T>
+    struct InPlaceType {explicit InPlaceType() = default;};
+    template <std::size_t idx>
+    struct InPlaceIndex {explicit InPlaceIndex() = default;};
+    
+    // inline 变量需要 C++17
+    #if HasCpp17
+    constexpr inline InPlace inPlace{};
+    template <class T>
+    constexpr inline InPlaceType<T> inPlaceType{};
+    template <std::size_t idx>
+    constexpr inline InPlaceIndex<idx> inPlaceIndex{};
+    #endif
+}
 
 #endif // MetaUtilsByYX
--- Utility/Variant.h
+++ Utility/Variant.h
@@ -1,7 +1,9 @@
 #ifndef VariantByYX
 #define VariantByYX
 
+#include <MetaUtils.h>
 #include <cstddef>
+#include <utility>
 
 namespace SevenTools {
     template <class... Types>
@@ -26,9 +28,22 @@ namespace SevenTools {
         // index，活跃成员的索引
         std::size_t idx;
     public:
+        constexpr Variant(): idx(0) {
+            new (&storage) TypeAtAlias<0, Types...> ();
+        }
+        template <std::size_t i, class... Args>
+        constexpr Variant(InPlaceIndex<i>, Args&&... args): idx(i) {
+            new (&storage) TypeAtAlias<0, Types...> (std::forward<Args>(args)...);
+        }
 
         constexpr std::size_t index() const {
             return idx;
+        }
+
+        template <std::size_t i>
+        constexpr TypeAtAlias<i, Types...> get() const {
+            // TODO: 实现异常类并在此通过 idx 检查
+            return *static_cast<TypeAtAlias<i, Types...>*>(&storage);
         }
     };
 }
